b5f65fa840ab3c005f2c526cfc0b5aeaa62377ca
Ben McManus <bennoislost@users.noreply.github.com>
2019-04-07 12:45:49 +0100

    CI fixes

--- src/TrainjunkiesPackages/QueueSubscriber/NationalRail/PushPortMessage.php
+++ src/TrainjunkiesPackages/QueueSubscriber/NationalRail/PushPortMessage.php
@@ -5,13 +5,11 @@ namespace TrainjunkiesPackages\QueueSubscriber\NationalRail;
 use TrainjunkiesPackages\QueueSubscriber\Queue\Message;
 use TrainjunkiesPackages\QueueSubscriber\Queue\MessageAbstract;
 
-class PushPortMessage
-    extends MessageAbstract
-    implements Message
+class PushPortMessage extends MessageAbstract implements Message
 {
     public function ack(callable $callback, $timeout = 0)
     {
-        $function = function($body, $headers) use ($callback) {
+        $function = function ($body, $headers) use ($callback) {
             $callback(
                 gzdecode($body),
                 $headers
--- src/TrainjunkiesPackages/QueueSubscriber/NetworkRail/DataFeedMessage.php
+++ src/TrainjunkiesPackages/QueueSubscriber/NetworkRail/DataFeedMessage.php
@@ -5,9 +5,7 @@ namespace TrainjunkiesPackages\QueueSubscriber\NetworkRail;
 use TrainjunkiesPackages\QueueSubscriber\Queue\Message;
 use TrainjunkiesPackages\QueueSubscriber\Queue\MessageAbstract;
 
-class DataFeedMessage
-    extends MessageAbstract
-    implements Message
+class DataFeedMessage extends MessageAbstract implements Message
 {
     public function ack(callable $callback, $timeout = 0)
     {
